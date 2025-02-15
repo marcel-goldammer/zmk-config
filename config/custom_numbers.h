a706dfa894dcbfb60c4bb1f8e1d068b3b8ac9f36
Marcel Goldammer <marcel.goldammer@web.de>
2025-02-15 10:16:36 +0100

    custom number workaround

--- config/custom_numbers.h
+++ config/custom_numbers.h
@@ -1,28 +1,24 @@
 /*
- * use US number keys for a german keyboard layout
+ * use custom US keys for a german keyboard layout
  * 1 -> !, 2 -> @, 3 -> #, 4 -> $, 5 -> %, 6 -> ^, 7 -> &, 8 -> *, 9 -> (, 0 -> )
  */
 #pragma once
 
-#define CUSTOM_NUMBER(name, num, sym) \
+#define CUSTOM_MAPPING(name, num, sym) \
   ZMK_MOD_MORPH(name, \
     bindings = <num>, <sym>; \
     mods = <(MOD_LSFT|MOD_RSFT)>; \
   )
 
-#define KP_CARET (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_GRAVE_ACCENT_AND_TILDE))
-#define KP_AMPS (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYPAD_AMPERSAND))
+// custom number keys
 #define KP_STAR (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYPAD_ASTERISK))
-#define KP_LPAR (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYPAD_LEFT_PARENTHESIS))
-#define KP_RPAR (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYPAD_RIGHT_PARENTHESIS))
-  
-CUSTOM_NUMBER(cn1, &kp N1, &kp EXCL)
-CUSTOM_NUMBER(cn2, &kp N2, &kp LA(L)) // LALT + L = @
-CUSTOM_NUMBER(cn3, &kp N3, &kp NON_US_HASH)
-CUSTOM_NUMBER(cn4, &kp N4, &kp DLLR)
-CUSTOM_NUMBER(cn5, &kp N5, &kp PRCNT)
-CUSTOM_NUMBER(cn6, &kp N6, &kp KP_CARET)
-CUSTOM_NUMBER(cn7, &kp N7, &kp LS(N6)) // LSHIFT + N6 = &
-CUSTOM_NUMBER(cn8, &kp N8, &kp KP_STAR)
-CUSTOM_NUMBER(cn9, &kp N9, &kp LS(N8)) // LSHIFT + N8 = (
-CUSTOM_NUMBER(cn0, &kp N0, &kp LS(N9)) // LSHIFT + N9 = )
+CUSTOM_MAPPING(cn1, &kp N1, &kp EXCL)
+CUSTOM_MAPPING(cn2, &kp N2, &kp LA(L)) // LALT + L = @
+CUSTOM_MAPPING(cn3, &kp N3, &kp NON_US_HASH)
+CUSTOM_MAPPING(cn4, &kp N4, &kp DLLR)
+CUSTOM_MAPPING(cn5, &kp N5, &kp PRCNT)
+CUSTOM_MAPPING(cn6, &kp N6, &kp GRAVE)
+CUSTOM_MAPPING(cn7, &kp N7, &kp LS(N6)) // LSHIFT + N6 = &
+CUSTOM_MAPPING(cn8, &kp N8, &kp KP_STAR)
+CUSTOM_MAPPING(cn9, &kp N9, &kp LS(N8)) // LSHIFT + N8 = (
+CUSTOM_MAPPING(cn0, &kp N0, &kp LS(N9)) // LSHIFT + N9 = )
